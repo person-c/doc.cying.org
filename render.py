9d6c56e2275fd4290f12a0d8eb0bb315a3378f9f
snowGlint <cosm.de.c@gmail.com>
2023-09-29 18:47:54 +0800

    fix render.py error

--- render.py
+++ render.py
@@ -5,6 +5,6 @@ file = os.listdir('.')
 toc = [f for f in file if f.endswith('.Rmd')]
 
 for f in toc:
-    subprocess.run(f'Rscript -e "knitr::knit({f})', shell=True)
-    subprocess.run(['pandoc', f, f'-o {f.replace("Rmd", "html")}',
+    subprocess.run(f'Rscript -e "knitr::knit({f})"', shell=True)
+    subprocess.run(['pandoc', f, '-o', f.replace("Rmd", "html"),
                     '--template=templates/default.html'])
