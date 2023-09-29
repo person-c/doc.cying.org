18ed231d229b5a83b789355bfcb7a944f538db4f
dzou <m2c.w@outlook.com>
2023-09-29 22:05:48 +0800

    pdate render.py

--- render.py
+++ render.py
@@ -6,5 +6,6 @@ toc = [f for f in file if f.endswith('.Rmd')]
 
 for f in toc:
     subprocess.run(f'Rscript -e \'knitr::knit("{f}")\'', shell=True)
-    subprocess.run(['pandoc', f, '-o', f.replace("Rmd", "html"),
+    subprocess.run(['pandoc', f.replace("Rmd", "md"),
+                    '-o', f.replace("Rmd", "html"),
                     '--template=templates/default.html'])
