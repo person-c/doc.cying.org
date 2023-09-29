4552aee7cb41d82986cb864b709652fd9568889c
snowGlint <cosm.de.c@gmail.com>
2023-09-29 18:54:49 +0800

    test render.py

--- render.py
+++ render.py
@@ -5,6 +5,6 @@ file = os.listdir('.')
 toc = [f for f in file if f.endswith('.Rmd')]
 
 for f in toc:
-    subprocess.run(f'Rscript -e "knitr::knit({f})"', shell=True)
+    subprocess.run(f'Rscript -e "knitr::knit(\'{f}\')"', shell=True)
     subprocess.run(['pandoc', f, '-o', f.replace("Rmd", "html"),
                     '--template=templates/default.html'])
