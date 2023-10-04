fd1439e5b7e60d5de53f851c20c8f50112625b7b
cw <m2c.w@outlook.com>
2023-10-04 23:36:55 +0800

    restrcture dir and use python to write list rmd file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# doc.cying.org
-A website of my notes and web exercise demo
+# Website of my notes
+
+This website is used to put some of my notes.
+
+This website is base on the R package 'knitr' and software 'pandoc'. All notea are written by quarto.
--- render.py
+++ render.py
@@ -1,12 +1,50 @@
-import os
 import subprocess
+import os
+path = ['web', 'note', 'slides']
 
-file = os.listdir('.')
-toc = [f for f in file if f.endswith('.Rmd')]
+for p in path:
+    f = open(f'{p}.rmd', 'x')
+    f.write(f'''---
+css: [static/css/link.css, static/css/font.css, static/css/list.css]
+---
 
-for f in toc:
-    subprocess.run(f'Rscript -e \'knitr::knit("{f}")\'', shell=True)
-    subprocess.run(['pandoc', f.replace("Rmd", "md"),
-                    '-o', f.replace("Rmd", "html"),
-                    '--template=templates/default.html'])
-    os.remove(f.replace("Rmd", "md"))
+<header>
+      <div class="nav-bg"></div>
+      <nav class="nav-bar">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="slides.html">Slides</a></li>
+          <li><a href="web.html">Web</a></li>
+          <li><a href="note.html">Note</a></li>
+        </ul>
+      </nav>
+</header>
+
+:::{{.blog-list}}
+<ul class='archive-list'>
+```{{r results='asis'}}
+#| echo: false
+if ("{p}" == "slides") {{
+    slides_dir <- list.dirs(path = "resource/slides", recursive = FALSE)
+    sprintf("<a href='%s/index.html'>%s</a>", slides_dir,
+    gsub("resource/slides/", "", slides_dir)) |>
+    cat(sep = "\n\n")
+}} else {{
+    file <- list.files(path = "resource/{p}",
+        pattern = "*.html", recursive = TRUE)
+    sprintf("<li><a href='resource/{p}/%s'>%s</a></li>",
+      file, gsub(".html", "", file)) |>
+    cat(sep = "\\n\\n")
+}}
+```
+</ul>
+:::'''
+            )
+    f.close()
+
+    subprocess.run(f'Rscript -e \'knitr::knit("{p}.rmd")\'', shell=True)
+    subprocess.run(['pandoc', f'{p}.md',
+                    '-o', f'{p}.html',
+                    '--template=templates/list.html'])
+    # os.remove(f'{p}.rmd')
+    os.remove(f'{p}.md')
